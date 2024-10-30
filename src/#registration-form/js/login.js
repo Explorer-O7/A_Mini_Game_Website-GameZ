694c10a00b8f998a69078b22600cc58fdd5228f2
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-10-30 13:49:55 +0530

    Update login.js

--- src/#registration-form/js/login.js
+++ src/#registration-form/js/login.js
@@ -9,7 +9,7 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPQIraXxdgo8UB3AKYF46LRXKTexeFAX0",
+  apiKey: "<API_KEY>",
   authDomain: "a-mini-game-website-gamez.firebaseapp.com",
   projectId: "a-mini-game-website-gamez",
   storageBucket: "a-mini-game-website-gamez.appspot.com",
